0109486720a8278077cbac63ffafa64e72de904d
ash <nengxuj@qq.com>
2022-12-25 20:32:23 +0800

    09 finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let array = Array.from(string.toUpperCase()).filter(char => /[a-zA-Z]/.test(char));
+    for (let i = 0; i < array.length / 2; i++) {
+        if (array[i] != array[array.length - 1 -i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
