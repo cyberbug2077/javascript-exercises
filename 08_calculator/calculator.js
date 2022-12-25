773b3eac064bb7ffb338c08c588c400fe6947e6d
ash <nengxuj@qq.com>
2022-12-25 19:31:26 +0800

    08 finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce(add, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return multiply(Array(exp).fill(base));
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let a = 0;
+  const array = Array(num).fill(1).map(() => ++a);
+  return multiply(array);
 };
 
 // Do not edit below this line
